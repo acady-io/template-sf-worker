3d8327f6684ce33cf7a16a832046b5ac3bbdfb8e
schab <christian.schab@wusoma.com>
2021-02-25 13:30:13 +0100

    use acady-task-worker

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,5 +1,5 @@
-import {TaskProcessor} from "./task-processor/task-processor";
 import DummyTask from "./tasks/dummy-task";
+import {TaskProcessor} from "acady-task-worker";
 
 const taskProcessor = new TaskProcessor();
 
